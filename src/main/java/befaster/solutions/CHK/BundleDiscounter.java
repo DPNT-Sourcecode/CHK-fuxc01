691e57555baa217cb5d698b4b80c1374785f19f7
sbx_user1051 <sbx_user1051@169.254.64.221>
2019-11-17 20:05:55 +0000

    Sun Nov 17 20:05:55 UTC 2019

---
+++ src/main/java/befaster/solutions/CHK/BundleDiscounter.java
@@ -0,0 +1,11 @@
+package befaster.solutions.CHK;
+
+import java.util.Map;
+
+public class BundleDiscounter implements Discounter {
+
+    @Override
+    public int getDiscount(Map<Character, Integer> counter) {
+        return 0;
+    }
+}
---
+++ src/main/java/befaster/solutions/CHK/Discounter.java
@@ -0,0 +1,7 @@
+package befaster.solutions.CHK;
+
+import java.util.Map;
+
+public interface Discounter {
+    int getDiscount(Map<Character, Integer> counter);
+}
