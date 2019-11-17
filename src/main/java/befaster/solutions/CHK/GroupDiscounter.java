1f9641887a244d36537f21be3f9231e86fa33462
sbx_user1051 <sbx_user1051@169.254.64.221>
2019-11-17 21:09:52 +0000

    Sun Nov 17 21:09:52 UTC 2019

--- src/main/java/befaster/solutions/CHK/GroupDiscounter.java
+++ src/main/java/befaster/solutions/CHK/GroupDiscounter.java
@@ -1,4 +1,31 @@
 package befaster.solutions.CHK;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class GroupDiscounter {
+
+    private final List<Character> items;
+    private int quantity;
+    private int price;
+
+    public GroupDiscounter(List<Character> items) {
+        this.items = items;
+    }
+
+    public int getTotalPrice(Map<Character, Integer> counter, Map<Character, Integer> priceMap) {
+        List<Character> sortedItems = items.stream()
+                .sorted(Comparator.comparingInt(priceMap::get).reversed())
+                .collect(Collectors.toList());
+        int residualItems = 0;
+        int regularPriceSum = 0;
+        int total = 0;
+        for (Character item: sortedItems) {
+            total += (counter.getOrDefault(item, 0) / quantity) *
+        }
+        return total;
+    }
 }
+
